32da9d1576dd727a8b218668cd57e43c2c1d2001
Radomir Radionov <66359081+radomir-radionov@users.noreply.github.com>
2020-10-27 22:16:28 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,103 @@
+// Функции
+
+// Задание 1
+
+// function func() {
+// 	alert('Радомир');
+// }
+
+// func()
+
+// Задание 2
+
+// function sumTo(n) {
+//     let sum = 0;
+//     for (let i = 1; i <= n; i++) {
+//       sum += i;
+//     }
+//     return sum;
+//   }
+  
+//   alert( sumTo(100) );
+
+// OR
+
+//   function sumTo(n) {
+//     if (n === 1) return 1;
+//     return n + sumTo(n - 1);
+//   }
+  
+//   alert( sumTo(100) );
+
+// Задание 3
+
+// function scuare(num) {
+//     alert(num ** 3)
+// }
+
+// scuare(2);
+
+// Задание 4
+
+// function scuare(num) {
+//     if (num >= 0) {
+//         alert('+++')
+//     } else (alert('---'))
+// }
+
+// scuare(-1);
+
+// Задание 5 
+
+// function sum(num1, num2, num3) {
+//     alert(num1 + num2 + num3)
+// }
+
+// sum(1,-2,3);
+ 
+// Задание 6 
+
+// function sum(param1, param2, param3) {
+//     alert(param1 + param2 + param3)
+// }
+
+// let param1 = 1;
+// let param2 = 2;
+// let param3 = 3;
+// sum(param1, param2, param3);
+
+// Задание 7
+
+// function func(num1 = 0, num2 = 0) {
+// 	alert(num1 + num2);
+// }
+// func()
+ 
+// Задание 8
+
+// function func(num) {
+//     return num ** 3
+// }
+
+// let result = func(4)
+// alert(result)
+
+// Задание 8 
+
+// function func(num) {
+// 	return  Math.sqrt(num);
+// }
+
+// let result = func(3) + func(4);
+// alert(result); 
+
+// Задание 8 
+
+let num = 2
+
+function sqrt(num) {
+	return Math.sqrt(num);
+	
+}
+
+let result = sqrt(2)
